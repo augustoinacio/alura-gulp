75e6868a79c7fbb8db645d00e5c1b0c99e20d64a
Augusto Inacio <augustoinacio@gmail.com>
2017-09-28 21:12:14 -0300

    Inclui usemin, cssmin, autoprefixer, etc

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,38 @@
 var gulp = require('gulp'),
     imagemin = require('gulp-imagemin'),
-    clean = require('gulp-clean');
+    clean = require('gulp-clean'),
+    concat = require('gulp-concat'),
+    htmlReplace = require('gulp-html-replace')
+    uglify = require('gulp-uglify'),
+    usemin = require('gulp-usemin'),
+    cssmin = require('gulp-cssmin');
+
 
 gulp.task('copy',['clean'],function(){
     return gulp.src('src/**/*')
     .pipe(gulp.dest('dist'));
-})
+});
 
 gulp.task('clean',function(){
-    gulp.src('dist')
+    return gulp.src('dist')
     .pipe(clean());
 });
 
-gulp.task('build-img', ['copy'], function(){
-    gulp.scr('dist/img/**/*')
-    .pipe(imagemin())
-    .pipe(gulp.dest('dist/img'));
-});    
+gulp.task('build-img', function() {
+      gulp.src('dist/img/**/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('dist/img'));
+});
 
+gulp.task('default',['copy'], function(){
+    gulp.start('build-img', 'usemin');
+});
 
+gulp.task('usemin', function(){
+    return gulp.src('dist/**/*.html')
+        .pipe(usemin({
+            'js': [uglify],
+            'css': [cssmin]
+        }))
+        .pipe(gulp.dest('dist'));
+});
